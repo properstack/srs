fccb79f42661bbc7c409642b3f379462b63f5b6a
Alex Walker <alensw11@gmail.com>
2016-12-21 16:22:20 -0500

    removed old migration

--- db/migrate/20161211033038_remove_j_from_books.rb
+++
@@ -1,6 +0,0 @@
-class RemoveJFromBooks < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :books, :name, :string
-    remove_column :books, :content, :text
-  end
-end
--- db/migrate/20161211033101_efer.rb
+++
@@ -1,4 +0,0 @@
-class Efer < ActiveRecord::Migration[5.0]
-  def change
-  end
-end
