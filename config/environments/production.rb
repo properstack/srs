8ad4ff34c3e08d8ff9eb68236bda4430108551f9
Alex Walker <alensw11@gmail.com>
2016-12-23 20:28:54 -0500

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+
+    config.assets.precompile += %w(cards.css decks_index.css cards_cheat.css)
+
 config.serve_static_assets = true
 
   # Code is not reloaded between requests.
