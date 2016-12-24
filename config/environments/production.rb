61354f6b609fb5b320f05d189052d947347a961b
Alex Walker <alensw11@gmail.com>
2016-12-23 21:44:48 -0500

    try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,7 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+config.assets.digest = true
 
     config.assets.precompile += %w(cards.css cards_cheat.css decks_index.css)
 
