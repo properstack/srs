2f760f5cdda16a70caca093bc598ed4ac58ad65c
Alex Walker <alensw11@gmail.com>
2016-12-23 20:11:12 -0500

    add sheets

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,9 @@ Bundler.require(*Rails.groups)
 
 module Srs
   class Application < Rails::Application
+
+    config.serve_static_assets = true
+
   	config.app_generators.scaffold_controller = :scaffold_controller
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
